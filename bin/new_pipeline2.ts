002e68d786bc8d9dcd799f6058722131140f501f
Chiapponi <luchiap@amazon.com>
2022-07-19 12:56:45 -0500

    initial commit

---
+++ bin/new_pipeline2.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { NewPipeline2Stack } from '../lib/new_pipeline2-stack';
+
+const app = new cdk.App();
+new NewPipeline2Stack(app, 'NewPipeline2Stack', {
+  env: {
+    account: '451255536186',
+    region: 'eu-east-1',
+  }
+});
+
+app.synth();
---
+++ test/new_pipeline2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NewPipeline2 from '../lib/new_pipeline2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/new_pipeline2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NewPipeline2.NewPipeline2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
